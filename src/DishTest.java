fc64511a9c55799065e606d8ea7b082232dff0e6
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2020-12-08 15:24:33 -0600

    working on oop mini ex

---
+++ src/DishTest.java
@@ -0,0 +1,14 @@
+public class DishTest {
+
+    public static void main(String[] args){
+
+        Dish dish1 = new Dish();
+        dish1.nameOfDish = "Kung Pao Chicken";
+        dish1.wouldRecommend = true;
+        dish1.costInCents = 1100;
+
+        dish1.printSummary();
+
+    }
+
+}
