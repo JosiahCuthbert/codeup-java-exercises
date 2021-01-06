9be57d305b573404e677594fd17df9177c40b4f0
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-01-06 15:45:31 -0600

    quiz notes

--- src/quiz.java
+++ src/quiz.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class quiz {
+
     public static void main(String[] args){
-        int x = 5 * 4 % 3;
-        String hello = "hello";
-        System.out.println(x+4);
+
+//        List<Integer> numbers = new ArrayList<>();
+//        numbers.add(13);
+//        numbers.add(new Integer(8)); // Line 7
+//        numbers.add(3.6); // Line 8
+//        for (Integer number: numbers) System.out.println(number); // Line 9
+
     }
 }
