f43767e9429a4e47ac8c763035b24df2feaffb0e
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-01-12 10:46:19 -0600

    notes from leetcode/codingbat exercises

--- src/quiz.java
+++ src/quiz.java
@@ -5,11 +5,60 @@ public class quiz {
 
     public static void main(String[] args){
 
-//        List<Integer> numbers = new ArrayList<>();
-//        numbers.add(13);
-//        numbers.add(new Integer(8)); // Line 7
-//        numbers.add(3.6); // Line 8
-//        for (Integer number: numbers) System.out.println(number); // Line 9
+        int[] arr1 = {-1, 3, 8, 2, 9, 5};
+        int[] arr2 = {4, 1, 2, 10, 5, 20};
+
+        for(int num : closest(arr1, arr2, 24)){
+            System.out.println(num);
+        }
+
+        test();
 
     }
+
+    public static int[] closest(int[] arr1, int[] arr2, int target){
+        Boolean start  = false;
+        int out1 = 0;
+        int out2 = 0;
+        int range = 0;
+        for(int num : arr1){
+            for(int i = 0; i<arr2.length;i++){
+                int instanceRange = nonNeg(target -  (arr2[i]+num));
+                if(!start || instanceRange < range){
+                    start = true;
+                    range = instanceRange;
+                    out1 = num;
+                    out2 = arr2[i];
+                }
+            }
+        }
+        return new int[]{out1, out2};
+    }
+
+    public static int nonNeg(int num){
+        if(num<0){
+            return num * -1;
+        }	else{
+            return num;
+        }
+
+
+    }
+
+    public static void test(){
+        String test = "test";
+        System.out.println(test.length());
+    }
+
+    public int wordsCount(String[] words, int len) {
+        int count = 0;
+        for(String word : words){
+            if(word.length() == len){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
